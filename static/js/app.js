1c9601ca07e470735c68dda7a6b205e32342329e
Bert Swinnen <bert.swinnen@gmail.com>
2016-01-08 16:12:45 +0100

    adds basic react stuff

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var Router = require('react-router-component');
+var Locations = Router.Locations;
+var Location = Router.Location;
+
+React.render(
+  document.getElementById('diy')
+);
